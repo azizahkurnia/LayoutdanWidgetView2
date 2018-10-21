f5b59c88228d4bf7dce41b2acb02326faece5f3b
azizahkurnia <mbakaziz25@gmail.com>
2018-10-21 08:23:37 +0700

    Layoutdanwidgetview2

---
+++ app/src/main/java/com/gmail/mbakaziz25/layoutdanwidgetview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.mbakaziz25.layoutdanwidgetview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
